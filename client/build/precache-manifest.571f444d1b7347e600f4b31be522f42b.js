98d47d42a7734f384e5f36e66e41e9cadda40d44
Anil E. Gunay <32689339+gunayanil@users.noreply.github.com>
2021-03-01 23:39:06 -0500

    build file

---
+++ client/build/precache-manifest.571f444d1b7347e600f4b31be522f42b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c56e092d4b6f32449b57bac05c016c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19175e0213cef2baff34",
+    "url": "/static/css/2.4858d7ec.chunk.css"
+  },
+  {
+    "revision": "19175e0213cef2baff34",
+    "url": "/static/js/2.057ffa78.chunk.js"
+  },
+  {
+    "revision": "d406cdd2ff777bb1780875f438877cc2",
+    "url": "/static/js/2.057ffa78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a1e50774a3883c2ed3",
+    "url": "/static/js/main.a3c7ddc8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/default-avatar.5a021ab9.png"
+  }
+]);
